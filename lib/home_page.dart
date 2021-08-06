0f40bf8dcb89bd12bb2107037d5a8b53441ebdef
furqanalishah <syedfurqan@wanclouds.net>
2021-08-06 23:24:22 +0500

    day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Center(
+        child: Container(child: Text("Welcome to the $days of flutter.")),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
